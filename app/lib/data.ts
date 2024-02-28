3cc11c5d512818078b91b65889885d8be113240f
Woziu <marcin.woz@hotmail.com>
2024-02-28 23:29:26 +0100

    streaming data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,11 +21,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 5000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 5 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -37,6 +37,10 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    console.log('Fetching latest invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Data fetch completed after 3 seconds.');
+    
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
